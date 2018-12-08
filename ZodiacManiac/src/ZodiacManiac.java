e477b5bac1d58914fa934cdcc4c65bd488195603
victoriaj1 <43707837+victoriaj1@users.noreply.github.com>
2018-12-08 12:04:10 -0500

    Update ZodiacManiac.java
    reworded sysout since year doesn't need to be 4 numbers long

--- ZodiacManiac/src/ZodiacManiac.java
+++ ZodiacManiac/src/ZodiacManiac.java
@@ -7,7 +7,7 @@ public class ZodiacManiac {
 
 		//reference # of days in each month
 		int[] bmonth = {31,28,31,30,31,30,31,31,31,30,31,30,31};
-		
+
 		int bmon=-1, bday=-1, year=-1; boolean leapy=false;
 
 		//while loop for birth year
@@ -25,32 +25,32 @@ public class ZodiacManiac {
 				}
 				catch(Exception e) {
 					//if input isn't integer, ask for new input
-					System.out.print("You have typed in a wrong character. Please type in a 4 digit Integer. \r\n");
+					System.out.print("You have typed in a wrong character. Please type in an Integer. \r\n");
 				}
 			}
 			//if byear isn't valid, prompt for new input
 			while(year==-1);
-			
+
 			if(year<1){
 				System.out.println("Error. Please enter a valid year.\r\n");
 			}
 		}
-		
-			//check if year is leap year
-			if((year%4==0) && (year%100!=0)||(year%400==0)){
-				leapy=true;
-			}
-			
-			//if year is leap year, February will have 29 days
-			if(leapy==true) {
-				bmonth[1]=29;
-			}
-			
+
+		//check if year is leap year
+		if((year%4==0) && (year%100!=0)||(year%400==0)){
+			leapy=true;
+		}
+
+		//if year is leap year, February will have 29 days
+		if(leapy==true) {
+			bmonth[1]=29;
+		}
+
 		//while loop for birth month
 		while(bmon>12||bmon<1) {	
 			System.out.println("Enter your birth month as an integer (Ex: July would be 07): ");
 
-			 //keep looping until input is integer
+			//keep looping until input is integer
 			do{
 				//check for if user input is integer
 				try {
@@ -60,12 +60,12 @@ public class ZodiacManiac {
 					bmon=Integer.parseInt(sbmon);
 				}
 				catch(Exception e) {
-					 //if input isn't integer, ask for new input
+					//if input isn't integer, ask for new input
 					System.out.print("Enter your birth month as an integer (Ex: July would be 07):");
 				}
 			}
 			while(bmon==-1);
-			
+
 			//if bmon isn't valid, prompt for new input
 			if(bmon>12||bmon<1) {
 				System.out.println("Error. Please reenter with a valid integer.\r\n");
@@ -76,13 +76,13 @@ public class ZodiacManiac {
 		while(bmonth[bmon-1]<bday||bday<1) {
 			System.out.println("Enter your birth date: ");
 
-			 //keep looping until input is integer
+			//keep looping until input is integer
 			do{
 				//check for if user input is integer
 				try {
 					//assign user input to string sbday
 					String sbday = sc.next();
-					 //attempt to convert string to int
+					//attempt to convert string to int
 					bday=Integer.parseInt(sbday);
 				}
 				catch(Exception e) {
@@ -91,7 +91,7 @@ public class ZodiacManiac {
 				}
 			}
 			while(bday==-1);
-			
+
 			//if bday isn't valid, prompt for new input
 			if(bmonth[bmon-1]<bday||bday<1) {
 				System.out.println("Error. Please reenter with a valid integer.\r\n");
